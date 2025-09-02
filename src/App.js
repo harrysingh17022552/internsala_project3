21b2617c52dc5d61683cf4cfd90e1a229853cfa9
Harish Nigam <harrysingh17022552@gmail.com>
2025-09-02 22:36:40 +0530

    Added new feature
    when user reloads page, it will fetch its current location and give weather condition according to its latitude and longitude.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,12 @@ import { FaSearch } from "react-icons/fa";
 import { useEffect, useRef, useState } from "react";
 function App() {
   const [weatherData, setWeatherData] = useState(data());
-  const [tempUnit, setTempUnit] = useState("celsius"); //fahrenheit or celsius
-  const [city, setCity] = useState("Delhi");
+  const [tempUnit, setTempUnit] = useState("fahrenheit"); //fahrenheit or celsius
+  const [city, setCity] = useState("");
+  const [coordinates, setCoordinates] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
   const [showDetails, setShowDetails] = useState(false);
   const [Details, setDetails] = useState({});
   const [loading, setLoading] = useState(true);
@@ -45,19 +49,70 @@ function App() {
       setLoading(false);
     }, 2000);
   };
+  const locationCoords = async () => {
+    setLoading(true);
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${
+        coordinates.latitude
+      }%2C${coordinates.longitude}/${getDate()}/${getDate(
+        3
+      )}?unitGroup=us&key=SNVNZ8F7LF75WRMNAG6C7F7AS&contentType=json`,
+      { method: "GET", headers: { "content-type": "application/json" } }
+    );
+    if (!response.ok) {
+      errorRef.current.textContent = `Something Wrong : ${response.status} : ${response.statusText}`;
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+
+    setWeatherData(data);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+  const getLocation = async () => {
+    function Location() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+    }
+
+    function success(position) {
+      setCoordinates({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    }
+
+    function error() {
+      alert("Sorry, no position available.");
+    }
+    Location();
+  };
   useEffect(() => {
-    searchCity();
+    getLocation();
   }, []);
+
+  useEffect(() => {
+    if (coordinates?.latitude && coordinates?.longitude) {
+      console.log("Coordinates updated:", coordinates);
+      locationCoords();
+    }
+  }, [coordinates]);
   return loading ? (
     <section className="flex flex-col gap-8 items-center">
       <h1>Loading...</h1>
-      <p ref={errorRef} className="text-red-600"></p>
+      <p ref={errorRef} className="text-red-600 text-4xl"></p>
     </section>
   ) : (
     <section className="min-w-screen flex flex-wrap flex-col sm:flex-row gap-10 p-4 box-border text-black">
       <article className="flex justify-center items-center w-full">
-        <div className="relative w-[98%] sm:w-[80%] flex items-center">
+        <div className="relative w-[98%] sm:w-[80%] md:w-[50%] flex items-center">
           <input
+            value={city}
             type="search"
             name="search"
             id="search"
@@ -66,7 +121,7 @@ function App() {
             onChange={(e) => setCity(e.target.value)}
           />
           <FaSearch
-            className="icon text-2xl absolute right-0 m-4"
+            className="icon text-2xl absolute right-0 m-8"
             onClick={searchCity}
           />
         </div>
@@ -76,27 +131,53 @@ function App() {
           <img
             src={
               weatherList().filter((item) =>
-                weatherData.currentConditions.conditions
+                weatherData.currentConditions.icon
                   .toLowerCase()
                   .includes(item.name.toLowerCase())
               )[0].image
             }
-            alt={weatherData.currentConditions.conditions}
+            alt={weatherData.currentConditions.icon}
             className="w-20 sm:w-40 rounded-lg"
           />
-          {tempUnit === "fahrenheit" ? (
-            <h1 className="text-5xl after:content-['°F']">
-              {Math.ceil(weatherData.currentConditions.temp)}
-            </h1>
-          ) : (
-            <h1 className="text-5xl after:content-['°C']">
-              {Math.ceil(((weatherData.currentConditions.temp - 32) * 5) / 9)}
-            </h1>
-          )}
+          <div className="flex gap-4">
+            {tempUnit === "fahrenheit" ? (
+              <h1 className="text-5xl after:content-['°F']">
+                {Math.ceil(weatherData.currentConditions.temp)}
+              </h1>
+            ) : (
+              <h1 className="text-5xl after:content-['°C']">
+                {Math.ceil(((weatherData.currentConditions.temp - 32) * 5) / 9)}
+              </h1>
+            )}
+            <div
+              className="bg-black w-14 h-7 self-center rounded-full flex items-center transition-all"
+              title="Toggle Temp Unit"
+            >
+              <p
+                className="bg-white aspect-square rounded-full w-6 transition-all cursor-pointer text-center text-red-600"
+                onClick={(e) => {
+                  if (tempUnit === "fahrenheit") {
+                    e.currentTarget.classList.add("translate-x-[130%]");
+                    e.currentTarget.textContent = "F";
+                    setTempUnit("celsius");
+                  } else {
+                    e.currentTarget.classList.remove("translate-x-[130%]");
+                    e.currentTarget.textContent = "C";
+                    setTempUnit("fahrenheit");
+                  }
+                }}
+              >
+                C
+              </p>
+            </div>
+          </div>
         </article>
         <article className="flex justify-between items-center">
           <div className="flex flex-col gap-2 whitespace-nowrap">
-            <strong>{weatherData.resolvedAddress.toUpperCase()}</strong>
+            <strong>
+              {weatherData.resolvedAddress.toUpperCase()} (
+              {weatherData.timezone})
+            </strong>
             <p className="whitespace-normal">{weatherData.description}</p>
             <p>TODAY, {getDate()}</p>
           </div>
@@ -105,8 +186,19 @@ function App() {
       <article className="flex flex-col gap-4 p-4 rounded-3xl border-2 sm:w-[calc(50%-40px)] grow">
         <article className="flex flex-nowrap justify-between items-center grow">
           <h1>TODAY</h1>
-          <IoIosRefresh className="icon" />
+          <IoIosRefresh
+            className="cursor-pointer"
+            onClick={(e) => {
+              e.currentTarget.classList.add("animate-spin");
+              setTimeout(() => {
+                city.length > 2 ? searchCity() : locationCoords();
+              }, 2000);
+            }}
+          />
         </article>
+        <strong className="text-center text-2xl uppercase text-gray-500 animate-pulse">
+          {weatherData.currentConditions.conditions}
+        </strong>
         <article className="flex flex-col gap-4 grow">
           <article className="flex flex-nowrap gap-4">
             <div className="flex flex-col items-center grow">
@@ -164,8 +256,17 @@ function App() {
       <article className="flex flex-col gap-12 p-4 rounded-3xl border-2 basis-full overflow-scroll noscrollbar">
         <article className="flex flex-nowrap justify-between items-center grow">
           <h1>Next 4 Days Forecast</h1>
-          <IoIosRefresh className="icon" />
+          <IoIosRefresh
+            className="cursor-pointer"
+            onClick={(e) => {
+              e.currentTarget.classList.add("animate-spin");
+              setTimeout(() => {
+                city.length > 2 ? searchCity() : locationCoords();
+              }, 2000);
+            }}
+          />
         </article>
+
         <article className="flex gap-2 justify-between">
           {weatherData.days.map((item, index) => (
             <div
@@ -178,9 +279,7 @@ function App() {
                 </strong>
                 {(() => {
                   const IconComponent = weatherList().filter((weather) =>
-                    item.conditions
-                      .toLowerCase()
-                      .includes(weather.name.toLowerCase())
+                    item.icon.toLowerCase().includes(weather.name.toLowerCase())
                   )[0];
 
                   if (IconComponent) {
@@ -213,7 +312,9 @@ function App() {
         {showDetails && (
           <article className="relative flex flex-col gap-4 p-4 rounded-3xl border-2 sm:w-[calc(75%-40px)] self-center grow">
             <article className="flex flex-col flex-nowrap justify-center items-center grow gap-4">
-              <h1 className="text-center text-3xl">{Details.conditions}</h1>
+              <h1 className="text-center text-3xl animate-pulse">
+                {Details.conditions}
+              </h1>
               <h1 className="text-center">{Details.description}</h1>
             </article>
             <article className="flex flex-col gap-4 grow">
@@ -264,12 +365,11 @@ function App() {
               </article>
             </article>
             <ImCross
-              className="icon absolute top-0 right-0 text-xl text-red-500"
+              className="absolute top-0 right-0 text-xl text-red-500 cursor-pointer"
               onClick={(e) => {
-                e.target.classList.add("animate-spin");
+                e.currentTarget.classList.add("animate-spin");
                 setTimeout(() => {
                   setShowDetails(false);
-                  e.target.classList.remove("animate-spin");
                 }, 2000);
               }}
             />
